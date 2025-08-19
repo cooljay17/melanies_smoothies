586b796807b838691c39978f35c04b81484b0302
Jayanthi Manickam Sethu <coolj1784@gmail.com>
2025-08-20 01:25:03 +0530

    missing col in select

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order=st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be',name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select((col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredient_list=st.multiselect(
